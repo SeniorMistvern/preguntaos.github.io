c873b59e67cce7532dc9a34ab6a908ff68d1cb2d
Blaziken132 <emigom132@gmail.com>
2024-07-04 08:24:38 -0600

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,31 @@ const questions = [
         answers: ["Gabriel García Márquez", "Mario Vargas Llosa", "Julio Cortázar", "Jorge Luis Borges"],
         correct: 0
     },
-    // Agrega más preguntas aquí
+    {
+        question: "¿Qué fue el Renacimiento y cómo transformó la cultura y el conocimiento en Europa?",
+        answers: ["Fue un movimiento cultural que revitalizó el arte", "Fue una guerra por la independencia de Estados Unidos", "Fue un movimiento religioso que causó la Reforma", "Fue una expedición a América"],
+        correct: 0
+    },
+    {
+        question: "¿Qué factores llevaron al colapso del Imperio Romano?",
+        answers: [" Invasiones bárbaras", "La invención de la imprenta", "La unificación de Alemania", "La construcción de las pirámides"],
+        correct: 0
+    },
+    {
+        question: "¿Cómo influyó la Ilustración en la Revolución Francesa y en el desarrollo de las democracias modernas?",
+        answers: ["Promovió ideas de igualdad", "Causó la caída del Imperio Romano", "Estableció el sistema feudal", "Fundó el Imperio Bizantino,"],
+        correct: 0
+    },
+    {
+        question: "¿Cuáles fueron las principales causas de la Primera Guerra Mundial?",
+        answers: ["Nacionalismo, alianzas militares, militarismo e imperialismo.", "La Guerra de Independencia de Estados Unidos, la peste negra, el descubrimiento de América.", "La caída de Constantinopla, la Revolución Rusa, el Renacimiento.", "La Revolución Francesa, el Tratado de Versalles, la Guerra de los Cien Años."],
+        correct: 0
+    },
+    {
+        question: "¿Qué impacto tuvo la Revolución Industrial en la sociedad y la economía del siglo XIX?",
+        answers: ["Aumento de la producción, urbanización y cambios en las condiciones laborales.", "La abolición de la esclavitud en Estados Unidos", "La independencia de América Latina", "La caída del Imperio Romano"],
+        correct: 0
+    },
 ];
 
 let currentQuestionIndex = 0;
